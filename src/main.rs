3a83b5de6e971b161adc8010d83fb898bbefc4f1
Sam Vevang <sam.vevang@gmail.com>
2015-08-12 23:23:10 -0500

    fixup server

--- src/main.rs
+++ src/main.rs
@@ -49,26 +49,13 @@ fn setup_database(conn:&Connection){
 
 }
 
-/*fn event_read(_: &mut Request) -> IronResult<Response> {
-    let ref name = _.extensions.get::<Router>().unwrap().find(":name").unwrap_or("/");
-    //Ok(Response::with((iron::status::Ok, format!("Hello: your event {}", name))));
-    //Ok(Response::with(iron::status::Ok, *name));
-    Ok(Response::with((iron::status::Ok, *name)));
-
-}*/
-
-fn event_read(req: &mut Request, db_conn: &Connection) -> IronResult<Response> {
+fn event_read(req: &mut Request) -> IronResult<Response> {
     let ref query = req.extensions.get::<Router>()
         .unwrap().find("name").unwrap_or("missing name param");
     println!("{}",query);
     Ok(Response::with((iron::status::Ok, *query)))
 }
 
-fn wrap_handlers(handler: fn(req: &mut Request, &postgres::Connection), db_conn: &Connection) -> fn(req: &mut Request) -> IronResult<Response> {
-
-
-    |req: &mut Request| -> IronResult<Response> {  handler(req, db_conn) }
-}
 fn main() {
 
     let conn = Connection::connect("postgres://sam@localhost/open_analytics_development", &SslMode::None)
@@ -76,11 +63,12 @@ fn main() {
 
     let mut router = Router::new();
 
-    //let mut chain = Chain::new(event_read);
-    let mut chain = Chain::new(wrap_handlers(event_read, &conn));
+    let mut chain = Chain::new(event_read);
     chain.link_before(ResponseTime);
     chain.link_after(ResponseTime);
 
     router.get("api/v1/:name", chain);
+    println!("starting server on localhost:3000");
     Iron::new(router).http("localhost:3000").unwrap();
+
 }
