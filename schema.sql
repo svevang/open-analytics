e918e3bd3c835101b6dcc8fbc7ec9b448287a6df
Sam Vevang <sam.vevang@gmail.com>
2015-08-17 15:17:13 -0500

    drop constraint

--- schema.sql
+++ schema.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS analytics;
 CREATE TABLE analytics (
   id         serial primary key,
   name       varchar(40),
-  data       jsonb,
-  CONSTRAINT production UNIQUE(name)
+  data       jsonb
 );
