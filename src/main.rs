1f9f8a089fa505cd62f6730f0ff3869bdea4035b
Sam Vevang <sam.vevang@gmail.com>
2015-08-19 07:30:23 -0500

    add rust_serialize, post method

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate router;
 extern crate time;
 extern crate r2d2;
 extern crate r2d2_postgres;
+extern crate rustc_serialize;
 
 
 use iron::prelude::*;
@@ -14,6 +15,7 @@ use iron::{BeforeMiddleware, AfterMiddleware, typemap};
 use time::precise_time_ns;
 use router::Router;
 use postgres::Connection;
+use rustc_serialize::json;
 
 use std::sync::Arc;
 struct ResponseTime;
@@ -38,7 +40,8 @@ impl AfterMiddleware for ResponseTime {
 struct Event {
     id: i32,
     name: String,
-    json: String
+    json: json::Json
+
 }
 
 
@@ -47,12 +50,15 @@ fn print_database(conn:&Connection){
     println!("hi there!");
     let stmt = conn.prepare("SELECT * FROM analytics").unwrap();
     for row in stmt.query(&[]).unwrap() {
+        let id:i32 = row.get::<_, i32>(0);
+        let name:String =  row.get::<_, String>(1);
+        let json=  row.get::<_, rustc_serialize::json::Json>(2);
+        println!("Found event {}, {}, {:?}", id, name, json);
         let event = Event {
-            id: row.get(0),
-            name: row.get(1),
-            json: row.get(2)
+            id: id,
+            name: name,
+            json: json
         };
-        println!("Found event {}", event.name);
     }
 
 }
@@ -66,18 +72,35 @@ fn event_read(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((iron::status::Ok, *query)))
 }
 
+fn event_write(req: &mut Request) -> IronResult<Response> {
+    let conn = req.extensions.get::<app::App>().unwrap().database.get();
+    print_database(&conn.unwrap());
+    let ref query = req.extensions.get::<Router>()
+        .unwrap().find("name").unwrap_or("missing name param");
+    println!("{}",query);
+    let resp = format!("Post: {}", *query);
+    Ok(Response::with((iron::status::Ok, resp)))
+}
+
 fn main() {
 
     let app = app::App::new();
 
     let mut router = Router::new();
+    let arc_app = Arc::new(app);
 
     let mut chain = Chain::new(event_read);
-    chain.link_before(app::AppMiddleware::new(Arc::new(app)));
+    chain.link_before(app::AppMiddleware::new(arc_app.clone()));
     chain.link_before(ResponseTime);
     chain.link_after(ResponseTime);
-
     router.get("api/v1/:name", chain);
+
+    let mut chain = Chain::new(event_write);
+    chain.link_before(app::AppMiddleware::new(arc_app.clone()));
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    router.post("api/v1/:name", chain);
+
     println!("starting server on localhost:3000");
     Iron::new(router).http("localhost:3000").unwrap();
 
